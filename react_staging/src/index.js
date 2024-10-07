cc0e18ec622ffeabd66274dba715099dbfad8a51
GhostClock <zhangyuanlaifen@gmail.com>
2024-10-07 18:35:53 +0800

    自定义初始化文件

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+// 引入react核心库
+import React from "react";
+// 引入ReactDOM
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// 引入App组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 渲染App到页面
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // 推荐的严格模式
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-reportWebVitals();
+)
