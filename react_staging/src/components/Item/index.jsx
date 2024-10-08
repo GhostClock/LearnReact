78d469f3102f93e2ae6f325d3a4fb0a16e2d3a43
GhostClock <zhangyuanlaifen@gmail.com>
2024-10-08 23:00:08 +0800

    鼠标移入\移出效果

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -2,15 +2,30 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+  state = {
+    mouse: false
+  }
+
+  handleMouse(isEnter) {
+    return () => {
+      this.setState({mouse: isEnter})
+    }
+  }
+
   render() {
     const { name, done } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li className={mouse? 'active' :'normal'} 
+        onMouseEnter={this.handleMouse(true)}
+        onMouseLeave={this.handleMouse(false)}>
         <label>
           <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+        <button className="btn btn-danger" 
+          style={{display: mouse? 'block': 'none'}}>删除
+        </button>
       </li>
     )
   }
