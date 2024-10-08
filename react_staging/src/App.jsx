a418ae45d242f64d31a886dd9f49aa4f8a8d284b
GhostClock <zhangyuanlaifen@gmail.com>
2024-10-08 22:10:28 +0800

    初始化数据展示

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -7,12 +7,22 @@ import './App.css'
 
 // 创建并暴露App组件
 export default class App extends Component {
+  // 初始化数据
+  state = {
+    todos: [
+      {id: '001', name: '吃饭', done: true},
+      {id: '002', name: '睡觉', done: true},
+      {id: '003', name: '撸代码', done: false},
+    ]
+  }
+
   render() {
+    const { todos } = this.state
     return (
         <div className="todo-container">
           <div className="todo-wrap">
             <Header />
-            <List />
+            <List todos={todos} />
             <Footer />
           </div>
         </div>
