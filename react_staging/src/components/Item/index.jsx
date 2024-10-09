58e4750112c02c1c02caa1ca07cc2b0d9e6b4fc3
GhostClock <zhangyuanlaifen@gmail.com>
2024-10-09 20:16:16 +0800

    删除一个todo

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -19,6 +19,13 @@ export default class Item extends Component {
       this.props.updateTodo(id, checked)
     }
   }
+  // 删除一个todo的回调
+  handleDelete = (id) => {
+    if (window.confirm('确定删除吗？？？')) {
+      // 通知App组件删除
+      this.props.deleteTodo(id)
+    }
+  }
 
   render() {
     const { id, name, done } = this.props
@@ -31,7 +38,7 @@ export default class Item extends Component {
           <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" 
+        <button onClick={() => this.handleDelete(id)} className="btn btn-danger" 
           style={{display: mouse? 'block': 'none'}}>删除
         </button>
       </li>
