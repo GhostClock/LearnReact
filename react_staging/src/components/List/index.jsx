78508fe7fe34715ed0c3ff8106aad152c7f39e68
GhostClock <zhangyuanlaifen@gmail.com>
2024-10-11 23:57:14 +0800

    github搜索案例_pubsub

--- react_staging/src/components/List/index.jsx
+++ react_staging/src/components/List/index.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import PubSub from 'pubsub-js'
 import './index.css'
 
 export default class List extends Component {
+
+  // 初始化状态
+  state = {
+    users: [], // 初始值为数据
+    isFirst: true, // 是否为第一次打开页面
+    isLoading: false, // 标识是否处于加载中
+    error: '', // 存储请求相关的错误信息
+  }
+
+  componentDidMount() {
+    this.token = PubSub.subscribe('SEARCH_RESULT', (_, data) => {
+      this.setState(data)
+    })
+  }
+  componentWillUnmount() {
+    PubSub.unsubscribe(this.token);
+  }
+
   render() {
-    const { users, isFirst, isLoading, error} = this.props
+    const { users, isFirst, isLoading, error} = this.state
     return (
         <div className="row">
           {
--- react_staging/src/components/Search/index.jsx
+++ react_staging/src/components/Search/index.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import PubSub from 'pubsub-js'
 
 export default class Search extends Component {
   search =  () => {
     // 获取用户的输入
     const { keyWordRef: { value: keyWord } } = this // 连续写解构+重命名
     if (!keyWord) {  return alert('请输入关键字')}
-
-    const { updateAppState } = this.props
-    // 发送请求前通知App更新状态
-    updateAppState({isFirst: false, isLoading: true})
+    
+    // 发送请求前通知List更新状态
+    PubSub.publish('SEARCH_RESULT', {isFirst: false, isLoading: true});
     // 发起网络请求
     axios.get(`/api/search/users?q=${keyWord}`).then(
       response => {
         const { data:{ items: users } } = response
-        // 请求回来后给App回调数据
-        updateAppState({isLoading: false, users})
+        // 请求回来后给List回调数据
+        PubSub.publish('SEARCH_RESULT', {isLoading: false, users});
       }, error => {
-        // 发生错误后给App回调数据
-        updateAppState({isLoading: false, users: [], error: error.message})
+        // 发生错误后给List回调数据
+        PubSub.publish('SEARCH_RESULT', {isLoading: false, users: [], error: error.message});
       }
     )
   }
